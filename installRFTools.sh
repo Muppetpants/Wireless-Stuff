5e33e9f45a045c6c35fe9cf1e12817bcdb8ef1cb
Jason Gray <103676092+Muppetpants@users.noreply.github.com>
2023-10-23 07:35:59 -0400

    Add files via upload

---
+++ installRFTools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Usage:  run this script (sudo ./installRFTools.sh)
+
+#Install depends
+sudo apt update
+sudo apt install hackrf libhackrf-dev libhackrf0 cubicsdr 
+
+sleep 1
+
+
+
---
+++ installWifiTools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Usage:  run this script (sudo ./installWifiTools.sh)
+
+#Install depends
+sudo apt update
+sudo apt install airgraph-ng gpsd gpsprune -y
+
+sleep 1
+
+
+
