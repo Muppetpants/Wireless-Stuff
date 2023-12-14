9b05380dd491327f48fd9c764fa40275311c2a3f
Jason Gray <103676092+Muppetpants@users.noreply.github.com>
2023-12-14 11:03:11 -0500

    Update installTools.sh
    Added repos

--- installTools.sh
+++ installTools.sh
@@ -16,4 +16,13 @@ mkdir /home/$USER/Desktop/Bluetooth_Tools
 git clone https://github.com/ZeroChaos-/blue_hydra.git /home/$USER/Desktop/Bluetooth_Tools
 git clone https://github.com/ZeroChaos-/blue_sonar.git /home/$USER/Desktop/Bluetooth_Tools
 
+wget https://github.com/Muppetpants/pcaps
+wget https://raw.githubusercontent.com/Muppetpants/KismetFiles/main/kismetSetup.sh
+git clone https://github.com/Muppetpants/Password-Lists.git
+wget https://raw.githubusercontent.com/Muppetpants/Password-Lists/main/Hak5.txt
+wget https://raw.githubusercontent.com/Muppetpants/Password-Lists/main/500-worst-passwords.txt
+git clone https://github.com/Muppetpants/apks.git
+git clone https://github.com/Muppetpants/customWLAN.git
+
+
 
