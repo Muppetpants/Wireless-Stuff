25a6900a0f649fe7a6d0ac631d0cedbae21a4a6c
Jason Gray <103676092+Muppetpants@users.noreply.github.com>
2024-01-31 10:38:12 -0500

    Update installTools.sh
    folder issue

--- installTools.sh
+++ installTools.sh
@@ -11,10 +11,10 @@ fi
 
 sudo sh -c "apt update && apt install hackrf libhackrf-dev libhackrf0 cubicsdr airgraph-ng gpsd gpsprune bluez bluez-test-scripts python3-bluez python3-dbus libsqlite3-dev ubertooth ruby-dev bundler git"
 
-mkdir Bluetooth_Tools
+mkdir -p Bluetooth_Tools/blue_hydra Bluetooth_Tools/blue_sonar
 # Clone Bluetooth Tool Repos
-git clone https://github.com/ZeroChaos-/blue_hydra.git Bluetooth_Tools/
-git clone https://github.com/ZeroChaos-/blue_sonar.git Bluetooth_Tools/
+git clone https://github.com/ZeroChaos-/blue_hydra.git Bluetooth_Tools/blue_hydra
+git clone https://github.com/ZeroChaos-/blue_sonar.git Bluetooth_Tools/blue_sonar
 
 git clone https://github.com/Muppetpants/pcaps.git
 wget https://raw.githubusercontent.com/Muppetpants/KismetFiles/main/kismetSetup.sh
