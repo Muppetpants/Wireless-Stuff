e2fa515acad052b75e4fd2be0fdf96d78553f67b
Jason Gray <103676092+Muppetpants@users.noreply.github.com>
2024-01-31 10:29:06 -0500

    Update installTools.sh
    cleanup

--- installTools.sh
+++ installTools.sh
@@ -9,18 +9,16 @@ if [[ $EUID -eq 0 ]]; then
    exit 1
 fi
 
-sudo sh -c "apt update && apt install hackrf libhackrf-dev libhackrf0 cubicsdr airgraph-ng gpsd gpsprune bluez bluez-test-scripts python3-bluez python3-dbus libsqlite3-dev ubertooth ruby-dev bundler"
+sudo sh -c "apt update && apt install hackrf libhackrf-dev libhackrf0 cubicsdr airgraph-ng gpsd gpsprune bluez bluez-test-scripts python3-bluez python3-dbus libsqlite3-dev ubertooth ruby-dev bundler git"
 
-mkdir /home/$USER/Desktop/Bluetooth_Tools
+mkdir Bluetooth_Tools
 # Clone Bluetooth Tool Repos
-git clone https://github.com/ZeroChaos-/blue_hydra.git /home/$USER/Desktop/Bluetooth_Tools
-git clone https://github.com/ZeroChaos-/blue_sonar.git /home/$USER/Desktop/Bluetooth_Tools
+git clone https://github.com/ZeroChaos-/blue_hydra.git Bluetooth_Tools/
+git clone https://github.com/ZeroChaos-/blue_sonar.git Bluetooth_Tools/
 
-wget https://github.com/Muppetpants/pcaps
+git clone https://github.com/Muppetpants/pcaps.git
 wget https://raw.githubusercontent.com/Muppetpants/KismetFiles/main/kismetSetup.sh
 git clone https://github.com/Muppetpants/Password-Lists.git
-wget https://raw.githubusercontent.com/Muppetpants/Password-Lists/main/Hak5.txt
-wget https://raw.githubusercontent.com/Muppetpants/Password-Lists/main/500-worst-passwords.txt
 git clone https://github.com/Muppetpants/apks.git
 git clone https://github.com/Muppetpants/customWLAN.git
 git clone https://github.com/BwithE/rpi-wpa2.git
